65bb177e5d232b5291c3fdb634a8ea8833e3f137
“Sailentium” <walktoparadise6@gmail.com>
2022-08-02 18:38:07 +0300

    products_page - started covering products page

--- src/test/java/tests/pages/LoginPage.java
+++ src/test/java/tests/pages/LoginPage.java
@@ -1,6 +1,11 @@
 package tests.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
     public By errorMessageFieldLocator = By.cssSelector("[data-test='error']");
@@ -8,4 +13,17 @@ public class LoginPage {
     public By passwordInputLocator = By.cssSelector("[data-test='password']");
     public By userNameInputLocator = By.cssSelector("[data-test='username']");
     public By logoFieldLocator = By.cssSelector(".login_logo");
+
+    public void fillLoginFrom(Map<String, String> loginData) {
+        if (loginData.containsKey("userName")) {
+            $(userNameInputLocator).sendKeys(Keys.CONTROL + "A");
+            $(userNameInputLocator).sendKeys(Keys.BACK_SPACE);
+            $(userNameInputLocator).sendKeys(loginData.get("userName"));
+        }
+        if (loginData.containsKey("password")) {
+            $(passwordInputLocator).sendKeys(Keys.CONTROL + "A");
+            $(passwordInputLocator).sendKeys(Keys.BACK_SPACE);
+            $(passwordInputLocator).sendKeys(loginData.get("password"));
+        }
+    }
 }
