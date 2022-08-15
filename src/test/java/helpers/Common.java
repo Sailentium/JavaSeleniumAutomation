409adc9991c0d1513199f6923e16d7f110ffbf1e
Sailentium <43683609+Sailentium@users.noreply.github.com>
2022-08-15 17:23:39 +0300

    parallel_test (#5)
     Update maven.yml

--- src/test/java/helpers/Common.java
+++ src/test/java/helpers/Common.java
@@ -1,16 +1,10 @@
 package helpers;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.ResourceBundle;
 
 public class Common {
     public static String getChromeVersion() {
-        String env = System.getProperty("chromeVersion");
-        if (StringUtils.isEmpty(env)) {
-            env = getConfigValue("chromeVersion");
-        }
-        return env;
+        return getConfigValue("chromeVersion");
     }
 
     public static String getConfigValue(String key) {
